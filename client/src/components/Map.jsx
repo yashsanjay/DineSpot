f5c4a87220904ff3f0bdaaf2a28c0d2655110054
YASH <yashsanjay.k21@iiits.in>
2025-02-24 12:06:54 +0530

    custom marker added

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+// Fix for missing marker icons
+const customMarker = new L.Icon({
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41], // default size
+  iconAnchor: [12, 41], // anchor point
+  popupAnchor: [1, -34], // popup position
+  shadowSize: [41, 41],
+});
 
 const Map = ({ latitude, longitude, address }) => {
   return (
@@ -13,7 +25,7 @@ const Map = ({ latitude, longitude, address }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[latitude, longitude]}>
+      <Marker position={[latitude, longitude]} icon={customMarker}>
         <Popup>{address}</Popup>
       </Marker>
     </MapContainer>
@@ -21,4 +33,3 @@ const Map = ({ latitude, longitude, address }) => {
 };
 
 export default Map;
-
