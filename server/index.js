7d578e6b5fc87e87d440e3fa3b1063a8b25c994d
YASH <yashsanjay.k21@iiits.in>
2026-03-14 00:07:02 +0530

    cors changed

--- server/index.js
+++ server/index.js
@@ -7,11 +7,7 @@ const cors = require("cors");
 
 dotenv.config();
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/restaurants", restaurantRoutes);
 app.use("/auth", authRoutes);
